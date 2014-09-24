6d649102951637be67c3a442b5e3affb3a0b4cf8
cryzed <cryzed@googlemail.com>
2014-09-24 02:37:24 +0200

    Made monkey patching static

--- seleniumrequests/__init__.py
+++ seleniumrequests/__init__.py
@@ -4,5 +4,11 @@ from .request import request
 
 
 # Monkey patch Selenium webdriver classes and make them easily importable
-for class_ in Firefox, Chrome, Ie, Opera, Safari, PhantomJS, Android, Remote:
-    class_.request = request
+Firefox.request = request
+Chrome.request = request
+Ie.request = request
+Opera.request = request
+Safari.request = request
+PhantomJS.request = request
+Android.request = request
+Remote.request = request
