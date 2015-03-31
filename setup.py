80d0357d8bb4776d4497c66e51241d4beacf1df3
cryzed <cryzed@googlemail.com>
2015-03-31 23:55:22 +0200

    Version bump to 1.0.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name='selenium-requests',
-    version='1.0.1',
+    version='1.0.2',
     description='Extends Selenium WebDriver classes to include the request function from the Requests library, while doing all the needed cookie and request headers handling.',
     long_description=long_description,
     author='Chris Braun',
