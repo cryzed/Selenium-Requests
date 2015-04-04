00a4d0c76e691a3a2501f806f9f9d4df99798644
cryzed <cryzed@googlemail.com>
2015-04-04 21:00:28 +0200

    DRY testing

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name='selenium-requests',
-    version='1.1.0',
+    version='1.1.1',
     description='Extends Selenium WebDriver classes to include the request function from the Requests library, while doing all the needed cookie and request headers handling.',
     long_description=long_description,
     author='Chris Braun',
